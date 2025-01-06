a79cd3392828e5648074d0ffeff13cd7677b1162
Hibah <hibahmohammed.k@gmail.com>
2025-01-06 16:57:29 +0530

    till product details page completed

--- footvogueapp/urls.py
+++ footvogueapp/urls.py
@@ -10,9 +10,29 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('send-otp/', views.send_otp, name='send_otp'),
     path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('product/<int:product_id>/', views.product_details, name='product_details'),
+    
+    
+    ### admin urls ####
 
     path('admin_dash/', views.admin_dash, name='admin_dash'),
-    path('admin_users/', views.admin_user_management, name='admin_users'),
+    path('user_management/', views.user_management, name='user_management'),
+    path('block_user/<int:user_id>/', views.block_user, name='block_user'),
+    path('unblock_user/<int:user_id>/', views.unblock_user, name='unblock_user'),
     
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/add/', views.add_category, name='add_category'),
+    path('categories/edit/<int:category_id>/', views.edit_category, name='edit_category'),
+    path('categories/delete/<int:category_id>/', views.delete_category, name='delete_category'),
+
+    path('view-products/', views.view_products, name='view_products'),
+    path('products/add/', views.add_product, name='add_product'),
+    path('products/edit/<int:pk>/', views.edit_product, name='edit_product'),
+    path('products/delete/<int:pk>/', views.delete_product, name='delete_product'),
+
+
+
+
+
 ] 
  
